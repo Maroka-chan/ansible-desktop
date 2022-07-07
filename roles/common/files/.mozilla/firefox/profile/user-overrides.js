eb1ce2cc82ca79a1374b50172a220305213c2deb
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2022-07-07 02:42:10 +0200

    Add user-overrides.js

---
+++ roles/common/files/.mozilla/firefox/profile/user-overrides.js
@@ -0,0 +1,5 @@
+// My Overrides
+user_pref("browser.startup.page", 3);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("gfx.webrender.software", true);              // Utilize CPU because everything lags with Nvidia drivers
+user_pref("keyword.enabled", true);                     // Enable so the search bar uses search engine
